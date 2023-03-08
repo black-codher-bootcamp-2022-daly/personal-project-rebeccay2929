ba32678323bb0a300315b3fd5d7f0b820c82b703
Rebecca Boateng <r.y.boateng@outlook.com>
2023-03-08 16:10:35 +0000

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,21 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
+import Post from "./Post";
+import Header from "./Header";
+import Layout from "./Layout";
+import { Route, Routes } from "react-router-dom";
 
 // SERVICES THAT CALL OUR API ENDPOINTS
-import { getAllProfiles } from "./services/profileService";
+// import { getAllProfiles } from "./services/profileService";
 
 function App() {
-  const [profiles, setProfiles] = useState(null);
-
-  useEffect(() => {
-    async function getProfiles() {
-      if (!profiles) {
-        const response = await getAllProfiles();
-        setProfiles(response);
-      }
-    }
-
-    getProfiles();
-  }, [profiles]);
-
-  const renderProfile = (user) => {
-    return (
-      <li key={user._id}>
-        <h3>
-          {`${user.first_name} 
-          ${user.last_name}`}
-        </h3>
-        <p>{user.location}</p>
-      </li>
-    );
-  };
-
   return (
-    <div>
-      <ul>
-        {profiles && profiles.length > 0 ? (
-          profiles.map((profile) => renderProfile(profile))
-        ) : (
-          <p>No profiles found</p>
-        )}
-      </ul>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path={"/login"} element={<div> login page</div>} />
+      </Route>
+    </Routes>
   );
 }
-
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<React.StrictMode> <BrowserRouter> <App /> </BrowserRouter></React.StrictMode>);
 

3cce667d5aa80060015af3a10e4d1dba53978bcd
Rebecca Boateng <r.y.boateng@outlook.com>
2023-03-18 18:13:59 +0000

    login creds

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,4 +1,28 @@
+import { useState } from "react"
+
 export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+async function login(ev) {
+    ev.preventDefault();
+    const response =  await fetch ('http://localhost:8080/login', { 
+
+    method: 'POST',
+     body: JSON.stringify({username,password}),
+     headers: {'Content-Type': 'application/json'},
+    });
+    if (response.ok) {
+      setRedirect(true);
+
+    } else {
+      alert ('Incorrect credentials');
+    }
+  }
+  if (redirect) {
+    return <Navigate to ={'/'}/>
+  } 
+   
     return (
         <div>
           <form className="login">

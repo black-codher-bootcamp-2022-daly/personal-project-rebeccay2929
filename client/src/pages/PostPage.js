ec2b0bb76532a2456626c3585bf2c9dfd1e9d0fb
Rebecca Boateng <r.y.boateng@outlook.com>
2023-03-22 20:03:46 +0000

    edit button

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { formatISO9075 } from "date-fns";
+import { UserContext } from "../UserContext";
+import { Link } from "react-router-dom";
 
 export default function PostPage() {
   const [postInfo, setPostInfo] = useState(null);
+  const { userInfo } = useContext(UserContext);
   const { id } = useParams();
   useEffect(() => {
     fetch(`http://localhost:8080/post/${id}`).then((response) => {
@@ -23,9 +26,17 @@ export default function PostPage() {
       <h1>{postInfo.title}</h1>
       <time> {formatISO9075(new Date(postInfo.createdAt))}</time>
 
+      <div className=" edit"> </div>
+      <div className="edit-row">
+        <a className="edit-btn" href="">
+          Edit this post
+        </a>
+      </div>
+
       <div className="image">
         <img src={`http://localhost:8080/${postInfo.cover}`} alt="" />
       </div>
+
       <div
         className="content"
         dangerouslySetInnerHTML={{ __html: postInfo.content }}
